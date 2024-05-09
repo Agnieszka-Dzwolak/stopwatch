d9dd224b407b4eac7313774974d299b9ad06da44
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-05-09 21:10:09 +0200

    project done

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = {
+    milliseconds: 0,
+    seconds: 0,
+    minutes: 0,
+    intervalId: null,
+    oldSeconds: 0,
+    oldMinutes: 0,
+};
+
+export default data;
---
+++ src/dom.js
@@ -0,0 +1,8 @@
+const dom = {
+    time: document.querySelector('.time'),
+    start: document.getElementById('start'),
+    stop: document.getElementById('stop'),
+    reset: document.getElementById('reset'),
+};
+
+export default dom;
